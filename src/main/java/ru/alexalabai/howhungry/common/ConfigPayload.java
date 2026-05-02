0432f4db6de1e590e04c22ceb727d9c2f4b317ec
Sanya Alabai <70968297+sanyaalabai@users.noreply.github.com>
2026-05-02 13:44:29 +0300

    1.21.5

--- src/main/java/ru/alexalabai/howhungry/common/ConfigPayload.java
+++ src/main/java/ru/alexalabai/howhungry/common/ConfigPayload.java
@@ -8,8 +8,8 @@ import net.minecraft.network.packet.CustomPayload;
 public record ConfigPayload(boolean serverEnabled, boolean serverHungerEnabled, int serverRunHunger) implements CustomPayload {
     public static final Id<ConfigPayload> ID = new Id<>(ModPackets.CONFIG_SYNC_PACKET);
     public static final PacketCodec<RegistryByteBuf, ConfigPayload> CODEC = PacketCodec.tuple(
-            PacketCodecs.BOOL, ConfigPayload::serverEnabled,
-            PacketCodecs.BOOL, ConfigPayload::serverHungerEnabled,
+            PacketCodecs.BOOLEAN, ConfigPayload::serverEnabled,
+            PacketCodecs.BOOLEAN, ConfigPayload::serverHungerEnabled,
             PacketCodecs.INTEGER, ConfigPayload::serverRunHunger,
             ConfigPayload::new
     );
